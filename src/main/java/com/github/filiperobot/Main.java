93c7f1c9adf9abfe7032d8695c952b929a5e4e56
Filipe Lemos <filipe_lemos@live.com>
2023-03-27 13:33:35 -0300

    first commit

---
+++ src/main/java/com/github/filiperobot/Main.java
@@ -0,0 +1,18 @@
+package com.github.filiperobot;
+
+import com.github.filiperobot.model.ClienteHttp;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        // fazer uma conexão HTTP e buscar os top 250 filmes
+        ClienteHttp cliente = new ClienteHttp("https://gist.githubusercontent.com/lucasfugisawa/cbb0d10ee3901bd0541468e431c629b3/raw/1fe1f3340dfe5b5876a209c0e8226d090f6aef10/Top250Movies.json");
+
+        System.out.println(cliente.getBody());
+
+        // extrair só os dados que interessam (titulo, poster, classificação)
+
+        // exibir e manipular os dados
+    }
+}
---
+++ src/main/java/com/github/filiperobot/model/ClienteHttp.java
@@ -0,0 +1,35 @@
+package com.github.filiperobot.model;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ClienteHttp {
+    private final String json;
+
+    private final HttpResponse<String> httpResponse;
+
+    public ClienteHttp(String url){
+
+        try {
+            HttpClient httpClient = HttpClient.newHttpClient();
+            HttpRequest httpRequest = HttpRequest.newBuilder(URI.create(url)).GET().build();
+
+            this.httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+
+            this.json = this.httpResponse.body();
+        } catch (IOException | InterruptedException exception) {
+            throw new RuntimeException(exception);
+        }
+    }
+
+    public String getBody(){
+        return json;
+    }
+
+    public HttpResponse<String> getHttpResponse() {
+        return httpResponse;
+    }
+}
